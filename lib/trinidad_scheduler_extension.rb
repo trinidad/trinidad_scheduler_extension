efb345d721acf422aaae60687519738126475cbb
kares <self@kares.org>
2012-03-22 09:14:32 +0100

    put jar requires into a separate file

--- lib/trinidad_scheduler_extension.rb
+++ lib/trinidad_scheduler_extension.rb
@@ -4,13 +4,10 @@ require 'trinidad'
 require 'trinidad/jars'
 
 # Jar files that are needed
-require 'trinidad_scheduler_extension/jars/log4j-1.2.16'
-require "trinidad_scheduler_extension/jars/slf4j-api-1.6.1"
-require "trinidad_scheduler_extension/jars/slf4j-log4j12-1.6.1"
-require "trinidad_scheduler_extension/jars/quartz-1.8.4"
+require 'trinidad_scheduler_extension/jars'
 
 # Trinidad Scheduler Extension files
-require 'trinidad_scheduler_extension/version.rb'
+require 'trinidad_scheduler_extension/version'
 require 'trinidad_scheduler_extension/trinidad_scheduler'
 require 'trinidad_scheduler_extension/extensions/object'
 require 'trinidad_scheduler_extension/scheduler_listener'
@@ -19,4 +16,3 @@ require 'trinidad_scheduler_extension/job_factory'
 require 'trinidad_scheduler_extension/scheduled_job'
 require 'trinidad_scheduler_extension/app_job'
 require 'trinidad_scheduler_extension/scheduler_extension'
-
---
+++ lib/trinidad_scheduler_extension/jars.rb
@@ -0,0 +1,4 @@
+require 'trinidad_scheduler_extension/jars/log4j-1.2.16'
+require "trinidad_scheduler_extension/jars/slf4j-api-1.6.1"
+require "trinidad_scheduler_extension/jars/slf4j-log4j12-1.6.1"
+require "trinidad_scheduler_extension/jars/quartz-1.8.4"
