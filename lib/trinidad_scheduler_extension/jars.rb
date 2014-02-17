785532944cba484af1608aaed69b6e20454b6f72
kares <self@kares.org>
2014-02-17 16:13:16 +0100

    update quartz (and it's jar dependencies) to 1.8.6

--- lib/trinidad_scheduler_extension/jars.rb
+++ lib/trinidad_scheduler_extension/jars.rb
@@ -1,5 +1,5 @@
 require 'java'
 
-['log4j-1.2.16','slf4j-api-1.6.1','slf4j-log4j12-1.6.1','quartz-1.8.4'].each do |jar|
+['log4j-1.2.17','slf4j-api-1.6.6','slf4j-log4j12-1.6.6','quartz-1.8.6'].each do |jar|
   load File.expand_path("../../trinidad-libs/#{jar}.jar", File.dirname(__FILE__))
 end
