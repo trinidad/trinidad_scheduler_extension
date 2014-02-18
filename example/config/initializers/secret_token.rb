0d6e3597307b04d9374b1b84d751ad098b26f44a
kares <self@kares.org>
2014-02-18 10:06:43 +0100

    setup an example rails application to show-case scheduler usage

---
+++ example/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchedulerExample::Application.config.secret_key_base = 'fe09f82219fd946b2f63ca230a7de4febe4b2700dcd7578b6d72516048f0f90130266e4f47d3919263b154a2cb58fc0b061e75277fd21f50f27cd6bc711d25cd'
---
+++ example/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SchedulerExample::Application.config.session_store :cookie_store, key: '_scheduler_example_session'
